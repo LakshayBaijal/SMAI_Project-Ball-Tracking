24bcbefa8cb95f19b876ad8093961089689dbaae
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2025-05-08 02:40:41 +0530

    Update train.py

--- train/train.py
+++ train/train.py
@@ -8,8 +8,7 @@ model = YOLO("yolov8n.pt")
 model.train(
     data="ttnet_ball.yaml",     
     epochs=20,                  
-    imgsz=640,
+    imgsz=320,
     batch=16,
-    project="TTBallProject",   
-    name="ball_yolo_model"     
+    project="ball_only",   
 )
