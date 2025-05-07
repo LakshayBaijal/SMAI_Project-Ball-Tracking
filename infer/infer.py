a71c89ef285248380c5e951ae47da6fe449d9056
Lakshay Baijal <104457846+LakshayBaijal@users.noreply.github.com>
2025-05-08 02:57:29 +0530

    Update infer.py

--- infer/infer.py
+++ infer/infer.py
@@ -1,12 +1,10 @@
 from ultralytics import YOLO
 
-# Load trained model
 model = YOLO("ball_only/train/weights/best.pt")
 
-# Run prediction on video
 results = model.predict(
-    source="videos/TT1.mp4",  # Provide video path or image
+    source="videos/TT1.mp4",  
     conf=0.25,
-    save=True,                        # Saves output video
+    save=True,                       
     project="ball_only",
 )
